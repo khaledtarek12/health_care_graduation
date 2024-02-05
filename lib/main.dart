e694fe31921d92becacb838fc43e1af35dbd78f4
khaledtarek12 <tareqkhalid092@gmail.com>
2024-02-05 23:18:54 +0200

    finish doctor pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import 'package:get/get.dart';
-import 'package:health_care/const.dart';
-import 'package:health_care/doctor_pages/views/doctor_homepage.dart';
-import 'package:health_care/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:health_care/screen_splash/views/splash_view.dart';
+import 'package:get/get.dart';
+
+import 'package:health_care/const.dart';
+import 'package:health_care/doctor_pages/views/doctor_homepage.dart';
+import 'package:health_care/firebase_options.dart';
 import 'package:health_care/login_and_signup/Screens/chat_page.dart';
-import 'login_and_signup/Screens/entier_doctor_chatcall_page.dart';
 import 'package:health_care/login_and_signup/Screens/login_home_page.dart';
+import 'package:health_care/screen_splash/views/splash_view.dart';
+
+import 'login_and_signup/Screens/entier_doctor_chatcall_page.dart';
 import 'login_and_signup/Screens/signup_home_page.dart';
 import 'login_and_signup/cubits/chat/chat_cubit.dart';
 import 'login_and_signup/cubits/login_cubit/login_cubit.dart';
